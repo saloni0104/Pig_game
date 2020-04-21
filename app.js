a6ca10dbc127d131847a7f439977995fc6c2b086
Saloni <suparekh01@gmail.com>
2020-04-21 22:06:26 +0530

    Roll dice activated

--- app.js
+++ app.js
@@ -9,19 +9,20 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer,dice;
+var scores, roundScore, activePlayer;
 
 
 scores =[0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
-document.getElementById('.score-0').style.display = 0;
-document.getElementById('.score-1').style.display = 0;
-document.getElementById('.current-0').style.display = 0;
-document.getElementById('.current-1').style.display = 0;
+
+document.getElementById('score-0').textContent ='0';
+document.getElementById('score-1').textContent ='0';
+document.getElementById('current-0').textContent ='0'
+document.getElementById('current-1').textContent ='0'
 
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
@@ -36,7 +37,34 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     diceDOM.src ='dice-' +dice + '.png';
 
     //3. Update the round score if the rolled number was not 1
+ 
+    if(dice !== 1) {
+        //Add score
+        roundScore +=dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+    }
+    else {
+        //Next Player
+        activePlayer === 0 ? activePlayer = 1 :activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        //document.querySelector('.player-0-panel').classList.add('active');
+        //document.querySelector('.player-1-panel').classList.remove('active');
+
+
+        document.querySelector('.dice').style.display = 'none';
+
+
+    }
 })
+
 
 
 
